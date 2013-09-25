f4c28b4fc5ff620d9938393bb7a3e94293ba8e85
polds <polds@kyanicorp.com>
2013-09-25 14:21:01 -0600

    Country fixes and Addtions
    Argentina - Fix
    Bulgaria - Add
    Cyprus - Add
    Croatia - Add

--- zip.go
+++ zip.go
@@ -8,17 +8,20 @@ type Zip string
 
 func (z Zip) patterns() map[string]string {
 	patterns := map[string]string{
-		"ar": "^\\d{4}$",
+		"ar": "^[A-Za-z]\\d{4}$",
 		"au": "^\\d{4}$",
 		"at": "^\\d{4}$",
 		"be": "^\\d{4}$",
+		"bg": "^\\d{4}$",
 		"br": "^\\d{5}[\\-]?\\d{3}$",
 		"ca": "^[A-Za-z]\\d[A-Za-z] \\d[A-Za-z]\\d$",
-		"dk": "^\\d{3,4}$",
+		"cy": "^\\d{4}$",
+		"dk": "^(2)|(3)\\d{2,3}$",
 		"de": "^\\d{5}$",
 		"es": "^((0[1-9]|5[0-2])|[1-4]\\d)\\d{3}$",
 		"gb": "^[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? \\d[ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}$",
 		"hu": "^\\d{4}$",
+		"hr": "^\\d{5}$",
 		"is": "^\\d{3}$",
 		"it": "^\\d{5}$",
 		"jp": "^\\d{3}-\\d{4}$",
