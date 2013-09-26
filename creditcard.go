a18b339112b6d2174082ea5b3b1742b16f27ff45
polds <polds@kyanicorp.com>
2013-09-26 00:49:30 -0600

    Add comments

--- creditcard.go
+++ creditcard.go
@@ -1,3 +1,7 @@
+// Package check is a validation library written in Go
+// to validate many real world applications.
+//
+// Examples include: emails, zip codes, phone numbers, etc.
 package check
 
 import (
@@ -18,6 +22,9 @@ func (c CreditCard) patterns() map[string]string {
 	return patterns
 }
 
+// CardIs function implements type CreditCard.
+// This function will validate if the value of CreditCard
+// is of the provided type.
 func (c CreditCard) CardIs(card string) bool {
 	if c.patterns()[card] == "" {
 		return false
@@ -26,6 +33,12 @@ func (c CreditCard) CardIs(card string) bool {
 	return c.match(c.patterns()[card], string(c))
 }
 
+// Merchant function implements CreditCard.
+// It will get the merchant (card type)
+// of a provided credit card number.
+//
+// If no card is found "Not a valid credit card" is returned.
+// @todo return string, error. Let user translate error themselves.
 func (c CreditCard) Merchant() string {
 	var out string = "Not a valid credit card"
 	if !c.CardIs("Any") {
