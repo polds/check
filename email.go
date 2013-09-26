7c91c63397396b1055892db1e8eecd089b46d5bf
polds <polds@kyanicorp.com>
2013-09-26 00:42:53 -0600

    Add comments

--- email.go
+++ email.go
@@ -1,3 +1,6 @@
+// Package check is a validation library written in Go
+// to validate many real world applications.
+// Examples include: emails, zip codes, phone numbers, etc.
 package check
 
 import (
@@ -6,7 +9,16 @@ import (
 
 type Email string
 
+// IsValid implements the Email type and
+// will validate if the value stored in Email is
+// a valid RFC compliant email address.
+// This returns a boolean value.
 func (e Email) IsValid() bool {
+
+	// BUG(polds) The following regex has issues
+	// validating the following types of emails (they return false):
+	// user@[IPv6:2001:db8:1ff::a0b:dbd0], postbox@com, and admin@mailserver1 .
+	// Which according to RFC 5322 should be valid addresses.
 	var pattern string = "^(([^<>()[\\]\\.,;:\\s@\"]+(\\.[^<>()[\\]\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$"
 
 	if string(e) == "" {
